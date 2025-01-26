bb5f4013a98b8d905685a296f5b425ddd723643d
Rémi Bastide <Remi.Bastide@irit.fr>
2025-01-26 19:58:12 +0100

    first commit

--- src/main/java/ssii/WebApp.java
+++ src/main/java/ssii/WebApp.java
@@ -12,22 +12,5 @@ public class WebApp {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApp.class, args);
 	}
-	
-	/**
-	 * Configure CORS for the application.
-	 * Permet l'accès à l'application depuis un autre domaine (Cross-Origin).
-	 * @return la configuation de CORS
-	 */
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*"); // for /** means all mapping URL, and * for all domain
-            }
-        };
-    }	
-
-	
 
 }
--- src/main/java/ssii/entity/Personne.java
+++ src/main/java/ssii/entity/Personne.java
@@ -13,6 +13,7 @@ import lombok.*;
 public class Personne {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer matricule;
 
     @NonNull
